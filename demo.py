4f9b3766b62dd55b2dba54156d40d7f2014d73ff
Rajan Khullar <rkhullar03@gmail.com>
2016-02-14 23:08:28 -0500

    changed minmax storage from list to object

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 """
 @author  :  Rajan Khullar
 @created :  02/07/16
-@updates :  02/07/16
+@updated :  02/14/16
 """
 
 dict = {}
@@ -9,6 +9,12 @@ dict = {}
 row = None
 sym = None
 val = None
+
+
+class Stat:
+    def __init__(self, v):
+        self.min = v
+        self.max = v
 
 with open('data.csv', 'r') as file:
     # skip header row
@@ -18,16 +24,16 @@ with open('data.csv', 'r') as file:
         row = line.split(';')
         sym = row[0].strip()
         val = int(row[1].strip())
-        # test for new symbolo
+        # test for new symbol
         if sym not in dict:
-            dict[sym] = [val, val]
+            dict[sym] = Stat(val)
         else:
             # test for new min
-            if val < dict[sym][0]:
-                dict[sym][0] = val
+            if val < dict[sym].min:
+                dict[sym].min = val
             # test for new max
-            if val > dict[sym][1]:
-                dict[sym][1] = val
+            if val > dict[sym].max:
+                dict[sym].max = val
 
 for sym in dict:
-    print("[{:1s}] [{:4d} : {:4d}]".format(sym, dict[sym][0], dict[sym][0]))
+    print("[{:1s}] [{:4d} : {:4d}]".format(sym, dict[sym].min, dict[sym].max))
